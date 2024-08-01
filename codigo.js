b0c44461347952776a6c56f3311b9743d08df038
ImTizy <161616301+ImTizy@users.noreply.github.com>
2024-08-01 12:15:59 -0300

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -1,32 +1,14 @@
-//declaracao de variaveis
-let nascimento
-let anoAtual
-let resultado
-let idadeCachorro
-let nome
-let qualCalculo
+let nota1
+let nota2
+let nota3
 
-nome = prompt("Qual o seu nome?");
-nascimento = parseInt(prompt("Qual a data de nascimento?"));
-anoAtual = parseInt(prompt("Qual o ano atual?"));
+nota1 = parseInt(prompt("Qual a noita do 1 trimestre?"))
+nota1 = parseInt(prompt("Qual a noita do 2 trimestre?"))
 
+resultado = 180 - (nota1+nota2);
 
-//primeiro desafio (idade humana)
-resultado = anoAtual - nascimento;
-
-//segundo desafio
-idadeCachorro = resultado * 7;
-
-//terceiro desafio (mostrar o nome)
-
-//quarto desafio (perguntar qual calculo o usuario quer)
-qualCalculo = parseInt(prompt("Digite 1 para idade humana e digite 2 para idade canina"));
-//mostrar o resultado
-if(qualCalculo ==1){
-    alert(nome + ", A idade é:  " + resultado);
-}
-else if(qualCalculo == 2){
-    alert(nome + ", A idade em anos caninos é:  " + resultado); 
-} else{
-    alert(nome + "Por favor digite um número valído");
+if(resultado> 0){
+    alert("Precisa de" + resultado + "para passar");
+} else {
+    alert("Parabéns! Você já está aprovado")
 }
