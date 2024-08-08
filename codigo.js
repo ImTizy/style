582cde16deee9efd1fa8076fa3f2e03f3928a592
Mathias192922 <163145663+Mathias192922@users.noreply.github.com>
2024-08-08 12:09:30 -0300

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -2,12 +2,26 @@
 let quantidade = 0;
 let total = 0;
 
+//função utilizada para calcular a quantidade...
 function Calculabruno(){
 
 //prompt para entrada de dados
 quantidade = parseInt(prompt("Digite a quantidade de vezes que foi preciso mandar ele calar a boca:"));
 
+//faz a soma e adicionar na variável total 
 total = total + quantidade;
 
-alert(total);
+//mostra o resultado usando alert
+//alert(total);
+
+//mostra o resultado usando innerHTML
+let dados = window.document.getElementById('contador');
+
+dados.innerHTML = "Quantidade: " +total + ".";
+
+if(total == 10){
+    dados.innerHTML = "Quantidade: " + total + " juliets de cria."
+}
+
+
 }
